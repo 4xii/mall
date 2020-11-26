e78e5f18487003556fc002c7f0a0506c5d33e45e
975795875@qq.com <975795875@qq.com>
2020-11-26 23:36:05 +0800

    json mock

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import storage from './storage/index'
+//import storage from './storage/index'
   export default {
     name:'app',
     components : {
@@ -13,15 +13,14 @@ import storage from './storage/index'
     },
     data(){
       return {
-     
+        res:{}
       }
     },
     mounted(){
-      //storage.setItem('a',1);
-      //storage.setItem('user',{a:1})
-      //storage.setItem('abc',{a:1},'user')
-      storage.clear('a','user')
-     
+      //本地加载请求静态json文件的形式
+      this.axios.get('/mock/user/login.json').then((res)=>{
+        this.res = res;
+      });
     }
   };
 </script>
