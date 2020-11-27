1091205c3ed4e368e47ef8719d87c0d5d8ab1335
975795875@qq.com <975795875@qq.com>
2020-11-27 11:07:52 +0800

    axios

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,17 @@
     },
     mounted(){
       //本地加载请求静态json文件的形式
-      this.axios.get('/mock/user/login.json').then((res)=>{
+      /* this.axios.get('/mock/user/login.json').then((res)=>{
+        this.res = res;
+      }); */
+
+      //通过easy-mock平台实现数据mock
+      /* this.axios.get('/user/login').then((res)=>{
+        this.res = res;
+      }); */
+
+      //本地集成mockjs实现数据mock
+      this.axios.get('/user/login').then((res)=>{
         this.res = res;
       });
     }
