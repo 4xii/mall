c97674bd408ef9278b04af629ba415f960387722
975795875@qq.com <975795875@qq.com>
2020-12-03 23:03:21 +0800

    Modal组件实现于交互

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,8 @@
     }
   };
 </script>
-<style>
-@import './assets/scss/reset.scss'
+<style lang="scss">
+@import './assets/scss/reset.scss';
+@import './assets/scss/config.scss';
+@import './assets/scss/button.scss';
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -50,16 +50,62 @@
 					<div class="swiper-button-next" slot="button-next"></div>
 				</swiper>
 			</div>
-			<div class="ads-box"></div>
-			<div class="banner"></div>
-			<div class="product-box"></div>
+			<div class="ads-box">
+				<a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+					<img :src="item.img" alt="">
+				</a>
+			</div>
+			<div class="banner">
+				<a href="/#/product/30">
+					<img src="/imgs/banner-1.png">
+				</a>
+			</div>
+		</div>
+		<div class="product-box">
+			<div class="container">
+				<h2>手机</h2>
+				<div class="wrapper">
+					<div class="banner-left">
+						<a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+					</div>
+					<div class="list-box">
+						<div class="list" v-for="(arr,i) in phoneList" :key="i">
+							<div class="item" v-for="(item,j) in arr" :key="j">
+								<span :class="{'new-pro':j%2==0}">新品</span>
+								<div class="item-img">
+									<img :src="item.mainImage" alt="">
+								</div>
+								<div class="item-info">
+									<h3>{{item.name}}</h3>
+									<p>{{item.subtitle}}</p>
+									<p class="price" @click="addCart(item.id)">{{item.price}}元</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
 		</div>
 		<service-bar></service-bar>
+		<modal 
+			title="提示"
+			suerText="查看购物车" 
+			btnType="1" 
+			modalType="middle" 
+			:showModal="showModal"
+			v-on:submit="goToCart"
+			v-on:cancel="showModal=false"
+			>
+			<template v-slot:body>
+				<p>商品添加成功</p>
+			</template>	
+		</modal>
 	</div>
 </template>
 
 <script>
 	import ServiceBar from './../components/ServiceBar'
+	import Modal from './../components/Modal'
 	import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
 	// import 'swiper/swiper-bundle.css'
 import 'swiper/css/swiper.css'
@@ -68,7 +114,8 @@ import 'swiper/css/swiper.css'
 			components:{
 				Swiper,
 				SwiperSlide,
-				ServiceBar
+				ServiceBar,
+				Modal
 			},
 			directives: {
        swiper: directive
@@ -144,7 +191,61 @@ import 'swiper/css/swiper.css'
 						[0,0,0,0],
 						[0,0,0,0],
 						[0,0,0,0],
-					]
+					],
+					adsList:[
+						{
+							id:33,
+							img:'imgs/ads/ads-1.png'
+						},
+						{
+							id:48,
+							img:'imgs/ads/ads-2.jpg'
+						},
+						{
+							id:45,
+							img:'imgs/ads/ads-3.png'
+						},
+						{
+							id:47,
+							img:'imgs/ads/ads-4.jpg'
+						},
+					],
+					phoneList:[
+						[1,1,1,1],
+						[1,1,1,1]
+					],
+					showModal:false
+				}
+			},
+			mounted(){
+				this.init();
+			},
+			methods:{
+				init(){
+					this.axios.get('/products',{
+						params:{
+							categoryId:100012,
+							pageSize:14
+						}
+					}).then((res)=>{
+						res.list = res.list.slice(6,14);
+						this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)];
+					})
+				},
+				addCart(){
+					this.showModal = true;
+					
+					/* this.axios.post('/carts',{
+						productId:id,
+						selected:true
+					}).then(()=>{
+
+					}).catch(()=>{
+						this.showModal = true;
+					}) */
+				},
+				goToCart(){
+					this.$router.push('/cart')
 				}
 			}
 	}
@@ -231,8 +332,103 @@ import 'swiper/css/swiper.css'
 					height: 100%;
 				}
 			}
-
-		}		
+		}
+		.ads-box{
+			@include flex();
+			margin-top:13px;
+			margin-bottom:31px;
+			a{
+				width: 296px;
+				height: 167px;
+			}
+		}
+		.banner{
+			margin-bottom:50px;
+		}
+		.product-box{
+			background-color: $colorJ;
+			padding-top:30px 0 50px;
+			h2{
+				font-size:$fontF;
+				height:21px;
+				line-height: 21px;
+				color:$colorB;
+				margin-bottom:20px;
+			}
+			.wrapper{
+				display:flex;
+				.banner-left{
+					margin-right: 16px;
+					img{
+						width: 224px;
+						height: 619px;
+					}
+				}
+				.list-box{
+					.list{
+						display:flex;
+						@include flex();
+						width: 986px;
+						margin-bottom:14px;
+						&:last-child{
+							margin-bottom: 0;
+						}
+						.item{
+							width: 236px;
+							height:302px;
+							background-color: $colorG;
+							text-align:center;
+							span{
+								display:inline-block;
+								width: 67px;
+								height: 24px;
+								font-size: 14px;
+								line-height: 24px;
+								color: $colorG;
+								&.new-pro{
+									background-color: #7ECF68;
+								}
+								&.kill-pro{
+									background-color: #E82626;
+								}
+								
+							}
+							.item-img{
+								img{
+									height: 195px;
+									width: 100%;
+								}
+							}
+							.item-info{
+								h3{
+									font-size: $fontJ;
+									color: $colorB;
+									line-height: $fontJ;
+									font-weight: bold;
+								}
+								p{
+									color:$colorD;
+									line-height: 13px;
+									margin:6px auto 13px;
+								}
+								.price{
+									color: #F20A0A;
+									font-size:$fontJ ;
+									font-weight: bold;
+									cursor:pointer;
+									&:after{
+										@include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+										content: '';
+										margin-left: 5px;
+										vertical-align:middle;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 </style>
