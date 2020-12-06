5668fffa7f6db9ed7fa99eb18961a3a927700828
975795875@qq.com <975795875@qq.com>
2020-12-06 12:10:07 +0800

    产品站组件动画模块的交互+修复getCartCount没取不到值的BUG

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,16 @@
     },
     mounted(){
       this.getUser();
-      this.CartCount()
+      this.getCartCount()
     },
     methods:{
       getUser(){
-        this.axios.get('/user').then((res)=>{
+        this.axios.get('/user').then((res={})=>{
           this.$store.dispatch('saveUserName',res.username);
         })
       },
       getCartCount(){
-        this.axios.get('/carts/products/sum').then((res)=>{
+        this.axios.get('/carts/products/sum').then((res=0)=>{
           this.$store.dispatch('saveCartCount',res);
         })
       }
